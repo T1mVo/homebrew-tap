0e6dafbea473df3f28c024382f7c9c34e12a3fe0
Tim Voßhenrich <94936637+T1mVo@users.noreply.github.com>
2025-06-27 11:08:46 +0200

    Add `incognito:0.1.1`

--- Formula/incognito.rb
+++ Formula/incognito.rb
@@ -1,8 +1,8 @@
 class Incognito < Formula
     desc "A zsh utility for temporarily disabling command history logging."
     homepage "https://github.com/T1mVo/incognito"
-    url "https://github.com/T1mVo/incognito/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "28e0e3512739167420ad6ecad67bffc404d31c096c5e4883a17a25564c2f56ab"
+    url "https://github.com/T1mVo/incognito/archive/refs/tags/v0.1.1.tar.gz"
+    sha256 "70a88bb14e6221dcef19e64c476cd99a8d991796c4ba692ce5ac5a9a81e1b2a0"
     license "MIT"
     
     def install
